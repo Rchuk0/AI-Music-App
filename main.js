cc71b3e9bca06461e1c00ec01f484633e6cb1868
Rchuk0 <81728499+Rchuk0@users.noreply.github.com>
2022-02-24 16:40:26 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ rightWrist_x = 0;
 rightWrist_y = 0;
 leftWrist_x = 0;
 leftWrist_y = 0;
+scoreleftWrist = 0;
+song_name = "";
 
 function setup(){
-    canvas = createCanvas(600,450);
-    canvas.position(330, 150);
-
+    canvas = createCanvas(600,430);
+    canvas.center();
+    canvas.position(325, 170);
 
     video = createCapture(VIDEO);
     video.hide();
@@ -24,6 +26,24 @@ function preload(){
 
 function draw(){
     image(video,0,0,600,530);
+
+    fill("#00ff00");
+    stroke("#ff0000");
+
+    song_name = Peter_pan_song.isPlaying();
+    console.log(song_name);
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        Harry_potter_theme_song.stop();
+        if(song_name == false){
+            Peter_pan_song.play();
+        }
+        else{
+            console.log("Song Name: Peter Pan Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Peter Pan Song";
+        }
+    }
 }
 
 function modelLoaded(){
@@ -33,7 +53,10 @@ function modelLoaded(){
 function gotposes(results){
     if(results.length > 0){
         console.log(results);
-         
+
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreleftWrist);
+
         leftWrist_x = results[0].pose.leftWrist.x;
         leftWrist_y = results[0].pose.leftWrist.y;
         console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
